444dbba30d7feedad95f756293989f2aa7bb5a1c
Melina <melina.dmec@gmail.com>
2026-03-24 12:48:29 +0100

    error corregido

--- assets/Js/scripts.js
+++ assets/Js/scripts.js
@@ -1,27 +1,30 @@
-
-const colorEstado = { r: 255, g: 255, b: 255 };
-
-// Array con las referencias a los 3 botones
-const misBotones = [
-  document.getElementById("btn-red"),
-  document.getElementById("btn-green"),
-  document.getElementById("btn-blue")
-];
-
-// --- Botón Verde 
+// BOTÓN VERDE 
 misBotones[1].onclick = () => {
-  colorEstado.g = Math.floor(Math.random() * 256);
-  document.body.style.backgroundColor =
-    "rgb(" + colorEstado.r + "," + colorEstado.g + "," + colorEstado.b + ")";
-  document.getElementById("color-text").textContent =
-    "rgb(" + colorEstado.r + ", " + colorEstado.g + ", " + colorEstado.b + ")";
+    // Generar número aleatorio para 'g'
+    colorEstado.g = Math.floor(Math.random() * 256);
+
+    // Crear color 
+    const nuevoColor = `rgb(${colorEstado.r}, ${colorEstado.g}, ${colorEstado.b})`;
+
+    // fondo
+    document.body.style.backgroundColor = nuevoColor;
+
+    // texto
+    textoColor.innerText = nuevoColor;
 };
 
-// --- Botón Azul 
+
+// BOTÓN AZUL 
 misBotones[2].onclick = () => {
-  colorEstado.b = Math.floor(Math.random() * 256);
-  document.body.style.backgroundColor =
-    "rgb(" + colorEstado.r + "," + colorEstado.g + "," + colorEstado.b + ")";
-  document.getElementById("color-text").textContent =
-    "rgb(" + colorEstado.r + ", " + colorEstado.g + ", " + colorEstado.b + ")";
+    // Generar número aleatorio para 'b'
+    colorEstado.b = Math.floor(Math.random() * 256);
+
+    // Crear color actualizado
+    const nuevoColor = `rgb(${colorEstado.r}, ${colorEstado.g}, ${colorEstado.b})`;
+
+    // fondo
+    document.body.style.backgroundColor = nuevoColor;
+
+    // texto
+    textoColor.innerText = nuevoColor;
 };
